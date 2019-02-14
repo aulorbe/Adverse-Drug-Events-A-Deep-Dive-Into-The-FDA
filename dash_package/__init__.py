af9fc6bb623461f2510700691462eda43d280e70
Audrey Sage Lorberfeld <aulorbe@gmail.com>
2019-02-14 10:20:58 -0500

    Cleaning up code

--- dash_package/__init__.py
+++ dash_package/__init__.py
@@ -1,25 +1,16 @@
 
 from flask import Flask
-
 import dash
+from flask_sqlalchemy import SQLAlchemy
+from dash_package.dashboard import *
+from dash_package.routes import *
+from dash_package.dashboard import *
 
 server = Flask(__name__)
 server.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///adverse-events.db'
 server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 server.config["SQLALCHEMY_ECHO"] = True
 
-
-from flask_sqlalchemy import SQLAlchemy
-
-
-
 db = SQLAlchemy(server)
 
-
 app = dash.Dash(__name__, server=server, url_base_pathname='/')
-
-
-
-from dash_package.dashboard import *
-from dash_package.routes import *
-from dash_package.dashboard import *
